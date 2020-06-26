799fe3c31f6bd1d6ea2d8c94e697654f9be1818c
Brandon Nichols <brandonaronnichols@gamil.com>
2020-06-25 23:05:49 -0500

    changed Procfile and package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./App.js";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
